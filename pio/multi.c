376123773a4986b181a86d73d1abbc5b9fcac2cb
schtromar <luka.macek@siol.net>
2022-11-20 19:20:28 +0100

    sync mode works

--- pio/multi.c
+++ pio/multi.c
@@ -5,24 +5,30 @@
 #include "hardware/pio.h"
 #include "hardware/irq.h"
 
-#include "edgeDetector.pio.h"
+#include "edgeDetector_sh.pio.h"
+#include "edgeDetector_ccd.pio.h"
 
 #define GPIO_ON  1
 #define GPIO_OFF 0
 
 #define LED_PIN 25
 
-#define SH_PIN 9
-//15
-#define PULSE_PIN 13
-//16
+#define CCD_CLOCK_PIN 0
+#define PULSE_PIN 1
+#define SH_PIN 2
 
 
-static uint count = 5555;
-uint64_t lastPulse;
+static uint64_t counter = 0;
+
+static uint64_t firstPulse;
+static uint64_t lastPulse;
+
+static uint64_t firstPulse_data;
+static uint64_t lastPulse_data;
 
 
-
+static int beeps = 0;
+static int boops = 0;
 
 void commThread(){
 	printf("Core 1 running...\n");
@@ -34,22 +40,78 @@ void commThread(){
 
 
 
-void risingEdgeInterrupt(){
+
+void PIOInterrupt(){
 	if(pio0_hw->irq & 1){
 		pio0_hw->irq = 1;
-		lastPulse = time_us_64();
 
-///		count = 0;
-//		gpio_put(LED_PIN, GPIO_ON);
+
+		counter++;
+		if(gpio_get(PULSE_PIN)){
+			if(firstPulse == 0){
+				firstPulse = counter;
+			}else{
+				lastPulse = counter;
+			}
+		}
+
+
+/*
+		beeps++;
+		if(beeps>10000){
+			printf("beep\n");
+			beeps = 0;
+		}
+*/
 	}else if(pio0_hw->irq & 2){
 		pio0_hw->irq = 2;
-//		gpio_put(LED_PIN, GPIO_OFF);
-//		uint data = count;
-//		multicore_fifo_push_blocking(data);
-		multicore_fifo_push_blocking(time_us_64()-lastPulse);
-///		multicore_fifo_push_blocking(count);
-///		count = 0;
 
+
+		multicore_fifo_push_blocking(firstPulse);
+		counter = 0;
+		firstPulse = 0;
+		lastPulse = 0;
+
+/*
+		boops++;
+		if(boops>10000){
+			printf("boop\n");
+			boops = 0;
+		}
+*/
+	}
+}
+
+void PIOInterrupt_sh(){
+	if(pio0_hw->irq & 2){			//SH
+		pio0_hw->irq = 2;
+printf("boop\n");
+
+		firstPulse_data = firstPulse;
+
+//		multicore_fifo_push_blocking(firstPulse_data);		//TODO: better, sredina mogoče?
+		multicore_fifo_push_blocking(counter);		//TODO: better, sredina mogoče?
+
+//		counter = 0;
+		firstPulse = 0;
+		lastPulse = 0;
+
+	}
+}
+
+void PIOInterrupt_ccd(){
+	if(pio0_hw->irq & 1){		//CCD
+		pio0_hw->irq = 1;
+//printf("beep\n");
+		if(gpio_get(PULSE_PIN)){
+			if(firstPulse == 0){
+				firstPulse = counter;
+			}else{
+				lastPulse = counter;
+			}
+		}
+
+		counter++;
 	}
 }
 
@@ -81,19 +143,47 @@ int main(){
 	printf("TEST CORE 1...\n");
 	multicore_fifo_push_blocking(1234);
 
-
-//	gpio_set_irq_enabled_with_callback(CLOCK_PIN, GPIO_IRQ_EDGE_RISE, true, &highCallback);
-
+	// Setup PIO
 	printf("PIO...");
-	PIO pio = pio0;
-	uint offset = pio_add_program(pio, &risingEdge_program);		//Load PIO program
-	uint sm = pio_claim_unused_sm(pio, &risingEdge_program);		//Find free PIO unit
-	risingEdgeInit(pio, sm, offset, SH_PIN, PULSE_PIN);			//Start PIO program
+/*
+	PIO pio_sh = pio0;
+	uint offset_sh = pio_add_program(pio_sh, &risingEdge_sh_program);		//Load PIO program
+	uint sm_sh = pio_claim_unused_sm(pio_sh, &risingEdge_sh_program);		//Find free PIO unit
+//	risingEdgeInit_sh(pio_sh, sm_sh, offset_sh, SH_PIN);				//Start PIO program
+	risingEdgeInit_sh(pio_sh, sm_sh, offset_sh, CCD_CLOCK_PIN);			//Start PIO program
 
-//	irq_add_shared_handler(PIO0_IRQ_0, risingEdgeInterrupt, PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);	//Add handler function for interrupt
-	irq_add_shared_handler(PIO0_IRQ_0, risingEdgeInterrupt, 0xff);	//Add handler function for interrupt
-	irq_set_enabled(PIO0_IRQ_0, true);										//Enable PIO0_IRQ_0 channel
-	pio0_hw->inte0 = PIO_IRQ0_INTE_SM0_BITS | PIO_IRQ0_INTE_SM1_BITS;						//Enable bits 0 and 1 in pio0_hw.inte0
+	PIO pio_ccd = pio0;
+	uint offset_ccd = pio_add_program(pio_ccd, &risingEdge_ccd_program);		//Load PIO program
+	uint sm_ccd = pio_claim_unused_sm(pio_ccd, &risingEdge_ccd_program);		//Find free PIO unit
+	risingEdgeInit_ccd(pio_ccd, sm_ccd, offset_ccd, CCD_CLOCK_PIN);			//Start PIO program
+
+	irq_add_shared_handler(PIO0_IRQ_1, PIOInterrupt_sh, 0xfe);		//Add handler function for interrupt
+	irq_set_enabled(PIO0_IRQ_1, true);					//Enable PIO0_IRQ_0 channel
+	pio0_hw->inte0 = PIO_IRQ1_INTE_SM0_BITS;				//Enable bits 0 and 1 in pio0_hw.inte0
+
+	irq_add_shared_handler(PIO1_IRQ_0, PIOInterrupt_ccd, 0xff);		//Add handler function for interrupt
+	irq_set_enabled(PIO1_IRQ_0, true);					//Enable PIO0_IRQ_0 channel
+	pio1_hw->inte0 = PIO_IRQ0_INTE_SM0_BITS;				//Enable bits 0 and 1 in pio0_hw.inte0
+*/
+
+	PIO pio = pio0;
+	uint offset_sh = pio_add_program(pio, &risingEdge_sh_program);			//Load PIO program
+	uint offset_ccd = pio_add_program(pio, &risingEdge_ccd_program);		//Load PIO program
+
+	uint sm_sh = pio_claim_unused_sm(pio, &risingEdge_sh_program);			//Find free PIO unit
+	uint sm_ccd = pio_claim_unused_sm(pio, &risingEdge_ccd_program);		//Find free PIO unit
+
+	risingEdgeInit_sh(pio, sm_sh, offset_sh, CCD_CLOCK_PIN);			//Start PIO program
+	risingEdgeInit_ccd(pio, sm_ccd, offset_ccd, CCD_CLOCK_PIN);			//Start PIO program
+
+	irq_add_shared_handler(PIO0_IRQ_0, PIOInterrupt, 0xff);				//Add handler function for interrupt
+	irq_set_enabled(PIO0_IRQ_0, true);						//Enable PIO0_IRQ_0 channel
+	irq_set_enabled(PIO0_IRQ_1, true);						//Enable PIO0_IRQ_0 channel
+	pio0_hw->inte0 = PIO_IRQ0_INTE_SM0_BITS | PIO_IRQ0_INTE_SM1_BITS;
+
+
+
+
 	printf("DONE\n");
 
 	sleep_ms(1000);
@@ -101,37 +191,19 @@ int main(){
 
 
 	printf("CLEARING PIO INTERRUPTS...");
+/*
+	pio_interrupt_clear(pio_sh, 0);
+	pio_interrupt_clear(pio_ccd, 0);
+*/
 	pio_interrupt_clear(pio, 0);
 	pio_interrupt_clear(pio, 1);
 	printf("DONE\n");
 
 	uint32_t interruptStatus;
 	printf("READY!\n");
-	while(true){
+	while(true){		// Trap. Only wait for interrupts now.
 
-/*
-		interruptStatus = save_and_disable_interrupts();
-		count++;
-//		sleep_us(1);
-		busy_wait_us(1);
-		restore_interrupts(interruptStatus);
-
-//		__asm volatile ("nop":);
-//		asmSleep();
-
-*/
 	}
 
-
-
-
-
-/*
-	while(1)
-	{
-		uint32_t command = multicore_fifo_pop_blocking();
-		gpio_put(LED_PIN, command);
-	}
-*/
 }
 
